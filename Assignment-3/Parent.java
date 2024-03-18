d0f969210f48d1ec93d46910150800b47901e001
Venicherla Bhargavi <123863188+Bhargavi-145@users.noreply.github.com>
2024-03-18 18:11:19 +0530

    Parent.java

---
+++ Assignment-3/Parent.java
@@ -0,0 +1,27 @@
+class Parent {
+    void display() {
+        System.out.println("Display method of the Parent class");
+    }
+}
+
+class Child extends Parent {
+    @Override
+    void display() {
+        System.out.println("Display method of the Child class");
+    }
+
+    void display(String message) {
+        System.out.println("Overloaded method: " + message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Parent parent = new Parent();
+        parent.display();
+
+        Child child = new Child();
+        child.display();
+        child.display("Hello from Child");
+    }
+}
